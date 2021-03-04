543c8a5293ccf2ad4ac64bfffc5896ac5028f91d
Gais El-AAsi <el-aasi@outlook.com>
2021-03-04 19:20:56 +0100

    Mini Project

--- Exercises.fs
+++ Exercises.fs
@@ -1,31 +1,31 @@
 ﻿module Exercises
 
 open System
+open System.IO
 
-//Ex 2.1 
+//Ex 2.1
 let x = 23
 let myName = "Bobby"
 let age = 25
 let country = "Canada"
 let y = 6 + 6
-
 let a = 5
 let b = let a = 10 in a + 5
 let c = a + b
 
-//Ex 2.2 
+//Ex 2.2
 let addNum1 x = x + 1
 let addNum10 x = x + 10
 let addNum20 x = (addNum10(0) * 2) + x
 
-//Ex 2.3 
+//Ex 2.3
 let max2 x y = if(x > y) then x else y
-let evenOrOdd x = if(x % 2 = 0) then printf("Event Number\n") else printf("Odd Number\n") 
-let addXY x y = printf($"First Number {x} \nSecond Number {y}\n Sum {x+y}\n") 
+let evenOrOdd x = if(x % 2 = 0) then printf("Event Number\n") else printf("Odd Number\n")
+let addXY x y = printf($"First Number {x} \nSecond Number {y}\n Sum {x+y}\n")
 
 //Ex 2.4
-let rec addNum_jk x y = 
-    if y = 0 then x 
+let rec addNum_jk x y =
+    if y = 0 then x
     else addNum10(0) + addNum_jk x (y-1)
 
 //Ex 2a.1a
@@ -38,26 +38,26 @@ let vowelToUpper = function
     |anyChar -> anyChar
 
 // 2a.1b
-let rec vowelToUpperInString (word : string) = 
-    match (Seq.toList word) with 
+let rec vowelToUpperInString (word : string) =
+    match (Seq.toList word) with
     |[] -> ""
     |sHead::sTail -> string (vowelToUpper sHead) + vowelToUpperInString (new string (sTail|> List.toArray))
 
 // 2a.2a
-let rec plcLength someList = 
+let rec plcLength someList =
     match someList with
     | [] -> 0
-    | lstHd::lstTail -> 1 + plcLength lstTail 
+    | lstHd::lstTail -> 1 + plcLength lstTail
 
 // 2a.2b
-let rec plcSum (someIntList : list<int>) = 
+let rec plcSum (someIntList : list<int>) =
     match someIntList with
     | [] -> 0
     | lstHead::lstTail -> lstHead + plcSum lstTail
 
 // 2a.3
-let rec takeSome numberOfElements listOfElements = 
-    match numberOfElements, listOfElements with 
+let rec takeSome numberOfElements listOfElements =
+    match numberOfElements, listOfElements with
     | 0, [] -> []
     | n, head::tail when n > 0 -> [head] @ takeSome (n-1) tail
 
@@ -68,7 +68,7 @@ let rec pclFold fn initialValue someList =
     | head::tail -> pclFold fn (fn initialValue head) tail
 
 // 2a.4b
-let rec plcSumWithFold (someIntList : list<int>) = 
+let rec plcSumWithFold (someIntList : list<int>) =
     match someIntList with
     | [] -> 0
     | theList -> pclFold (+) 0 theList
@@ -80,19 +80,19 @@ let rec pclFoldBack fn initialValue someList =
     | head::tail -> fn head (pclFoldBack fn initialValue tail)
 
 // 2a.5b
-let rec plcSumWithFoldBack (someIntList : list<int>) = 
+let rec plcSumWithFoldBack (someIntList : list<int>) =
     match someIntList with
     | [] -> 0
     | theList -> pclFoldBack (+) 0 theList
 
-// 2a.6 
-let rec plcIncList (someList : list<int>) = 
+// 2a.6
+let rec plcIncList (someList : list<int>) =
     match someList with
     | [] -> []
     | head::tail -> [head + 1] @ plcIncList tail
 
 // 2a.7a
-let rec plcMap fn someList = 
+let rec plcMap fn someList =
     match someList with
     | [] -> []
     | head::tail -> [fn head] @ plcMap fn tail
@@ -120,7 +120,7 @@ let isDivisible firstNumber secondNumber =
     secondNumber = firstNumber || secondNumber % firstNumber <> 0
 
 //Remove all the numbers that are divisible with each other
-let rec RemoveMultipliers firstListOfNumbers secondListOfNumbers = 
+let rec RemoveMultipliers firstListOfNumbers secondListOfNumbers =
     match firstListOfNumbers with
     | [] -> secondListOfNumbers
     | head :: tail -> RemoveMultipliers tail (List.filter (isDivisible head) secondListOfNumbers)
@@ -128,15 +128,15 @@ let rec RemoveMultipliers firstListOfNumbers secondListOfNumbers =
 //Get prime numbers up to
 let primesUpTo number = RemoveMultipliers [2 .. number] [1 .. number]
 
-//Ex 3.3 
-let rec plcFib number = 
+//Ex 3.3
+let rec plcFib number =
     match number with
     | n when n < 0 -> -1
     | 0 -> 0
     | 1 -> 1
     | m when m > 1 -> plcFib (number - 1) + plcFib(number - 2)
 
-//Ex 3.4 
+//Ex 3.4
 let doubleNum x = x * 2
 let sqrNum x = x * x
 
@@ -144,14 +144,13 @@ let plcQuad x = doubleNum (doubleNum x)
 let plcFourth x = sqrNum (sqrNum x)
 
 //TEST
-//Ex 1.a 
+//Ex 1.a
 let myInfoTuple = (279910, "Gais El-AAsi", "Moldova")
 
-//Ex 1.b 
-let printTuple someTuple = 
+//Ex 1.b
+let printTuple someTuple =
     match someTuple with
     | (a,b,c) -> printfn $"Student Number: {a}\nName: {b}\nCountry: {c}"
-
 
 let rec myStrLength (str : string) =
     match (Seq.toList str) with
@@ -159,3 +158,65 @@ let rec myStrLength (str : string) =
     | sHead::sTail -> 1 + myStrLength (new string [|for c in sTail -> c|])
 
 let usingMapOnList someList = someList |> List.map(fun x-> x+1)
+
+//TEST ATTEMPT 2
+
+//q1
+let triple1 = (12, 45, "AM")
+let triple2 = (4, 25, "PM")
+
+//q2
+let isBefore time1 time2 = 
+    match time1, time2 with
+    | (a, b, c), (d, e, f) when a > d && c = f -> false
+    | (a, b, c), (d, e, f) when a < d && c = f -> true
+    | (a, b, c), (d, e, f) when c = "PM" && f = "AM" -> false
+    | (a, b, c), (d, e, f) when c = "AM" && f = "PM" -> true
+    | (a, b, c), (d, e, f) when c = f && a = d && b > e -> false
+    | (a, b, c), (d, e, f) when c = f && a = d && b < e -> true
+
+//q3
+let isNthChar (str : string) (num : int) (chr : char) = if str.[num] = chr then true else false
+
+//q4
+let rec removeOdd listOfNumber = List.filter (fun x -> x % 2 = 0) listOfNumber 
+
+//4
+module WithoutRecords = 
+    type PlcShape<'a> =
+        | Rectangle of width : float * length : float
+        | RightTriangle of cathetus1 : float * cathetus2 : float 
+
+    let rect = Rectangle (width = 2.0, length = 2.5) 
+    let triangle = RightTriangle (cathetus1 = 3.0, cathetus2 = 2.0)
+
+    let plcArea shape = 
+        match shape with
+        | Rectangle(width, length) -> width * length
+        | RightTriangle(cathetus1, cathetus2) -> 0.5 * cathetus1 * cathetus2
+
+    let plcPerimeter shape = 
+        match shape with 
+        | Rectangle(width, length) -> 2.0 * width + 2.0 * length
+        | RightTriangle(cathetus1, cathetus2) -> cathetus1 + cathetus2 + sqrt (cathetus1 ** 2.0 + cathetus2 ** 2.0)
+
+module WithRecords =
+    type PclShape =  
+        | Rectangle of Shape
+        | RightTriangle of Shape
+    and Shape = { Height: float; Width: float }
+
+    let rect = Rectangle({Height = 2.0; Width=2.5})
+    let triangle = RightTriangle({Height = 2.0; Width = 3.0})
+    
+    let plcArea shape = 
+        match shape with
+        | Rectangle(shape) -> shape.Height * shape.Width
+        | RightTriangle(shape) -> 0.5 * shape.Height * shape.Width
+
+    let plcPerimeter shape = 
+        match shape with
+        | Rectangle(shape) -> 2.0*(shape.Height + shape.Width)
+        | RightTriangle(shape) -> shape.Height + shape.Width + sqrt(shape.Height**2.0 + shape.Width**2.0)
+
+ 
---
+++ MiniProject.fs
@@ -0,0 +1,95 @@
+﻿module MiniProject
+
+type Size = 
+    | Small
+    | Medium
+    | Large
+
+type DrinkType =
+    | Coffee
+    | Tea
+    | Juice
+    | Soda
+    | Milk
+
+type Drink = {
+    Size: Size
+    Type: DrinkType
+    Name: string
+    BasePrice: decimal
+} 
+
+let calculatePrice (drink:Drink) =
+    match drink.Size with
+    | Small -> drink.BasePrice
+    | Medium -> (drink.BasePrice * 1.25m)
+    | Large -> (drink.BasePrice * 1.4m)
+
+//Coffee Drinks
+let espressoS = {Size=Small; Type=Coffee; Name="Espresso Small"; BasePrice=12m}
+let espressoM = {Size=Medium; Type=Coffee; Name="Espresso Medium"; BasePrice=12m}
+let espressoL = {Size=Large; Type=Coffee; Name="Espresso Large"; BasePrice=12m}
+
+let caffeLatteS = {Size=Small; Type=Coffee; Name="Caffe Late Small"; BasePrice=23.5m}
+let caffeLatteM = {Size=Medium; Type=Coffee; Name="Caffe Late Medium"; BasePrice=23.5m}
+let caffeLatteL = {Size=Large; Type=Coffee; Name="Caffe Late Large"; BasePrice=23.5m}
+
+let americanoS = {Size=Small; Type=Coffee; Name="Americano Small"; BasePrice=17.25m}
+let americanoM = {Size=Medium; Type=Coffee; Name="Americano Medium"; BasePrice=17.25m}
+let americanoL = {Size=Large; Type=Coffee; Name="Americano Large"; BasePrice=17.25m}
+
+let latteMacchiatoS = {Size=Small; Type=Coffee; Name="Latte Macchiato Small"; BasePrice=27.75m}
+let latteMacchiatoM = {Size=Medium; Type=Coffee; Name="Latte Macchiato Medium"; BasePrice=27.75m}
+let latteMacchiatoL = {Size=Large; Type=Coffee; Name="Latte Macchiato Large"; BasePrice=27.75m}
+
+//Tea Drinks
+let greenTeaS = {Size=Small; Type=Tea; Name="Green Tea Small"; BasePrice=11.25m}
+let greenTeaM = {Size=Medium; Type=Tea; Name="Green Tea Medium"; BasePrice=11.25m}
+let greenTeaL = {Size=Large; Type=Tea; Name="Green Tea Large"; BasePrice=11.25m}
+
+let fruitTeaS = {Size=Small; Type=Tea; Name="Fruit Tea Small"; BasePrice=15.5m}
+let fruitTeaM = {Size=Medium; Type=Tea; Name="Fruit Tea Medium"; BasePrice=15.5m}
+let fruitTeaL = {Size=Large; Type=Tea; Name="Fruit Tea Large"; BasePrice=15.5m}
+
+let blackTeaS = {Size=Small; Type=Tea; Name="Black Tea Small"; BasePrice=10.75m}
+let blackTeaM = {Size=Medium; Type=Tea; Name="Black Tea Medium"; BasePrice=10.75m}
+let blackTeaL = {Size=Large; Type=Tea; Name="Black Tea Large"; BasePrice=10.75m}
+
+//Juice Drinks
+let appleJuiceS = {Size=Small; Type=Juice; Name="Apple Juice Small"; BasePrice=25.25m}
+let appleJuiceM = {Size=Medium; Type=Juice; Name="Apple Juice Medium"; BasePrice=25.25m}
+let appleJuiceL = {Size=Large; Type=Juice; Name="Apple Juice Large"; BasePrice=25.25m}
+
+let orangeJuiceS = {Size=Small; Type=Juice; Name="Orange Juice Small"; BasePrice=23.5m}
+let orangeJuiceM = {Size=Medium; Type=Juice; Name="Orange Juice Medium"; BasePrice=23.5m}
+let orangeJuiceL = {Size=Large; Type=Juice; Name="Orange Juice Large"; BasePrice=23.5m}
+
+let mangoJuiceS = {Size=Small; Type=Juice; Name="Mango Juice Small"; BasePrice=28.75m}
+let mangoJuiceM = {Size=Medium; Type=Juice; Name="Mango Juice Medium"; BasePrice=28.75m}
+let mangoJuiceL = {Size=Large; Type=Juice; Name="Mango Juice Large"; BasePrice=28.75m}
+
+//Soda Drink
+let cokeS = {Size=Small; Type=Soda; Name="Coke Small"; BasePrice=12m}
+let cokeM = {Size=Medium; Type=Soda; Name="Coke Medium"; BasePrice=12m}
+let cokeL = {Size=Large; Type=Soda; Name="Coke Large"; BasePrice=12m}
+
+let pepsiS = {Size=Small; Type=Soda; Name="Pepsi Small"; BasePrice=15m}
+let pepsiM = {Size=Medium; Type=Soda; Name="Pepsi Medium"; BasePrice=15m}
+let pepsiL = {Size=Large; Type=Soda; Name="Pepsi Large"; BasePrice=15m}
+
+let fantaS = {Size=Small; Type=Soda; Name="Fanta Small"; BasePrice=13m}
+let fantaM = {Size=Medium; Type=Soda; Name="Fanta Medium"; BasePrice=13m}
+let fantaL = {Size=Large; Type=Soda; Name="Fanta Large"; BasePrice=13m}
+
+let spriteS = {Size=Small; Type=Soda; Name="Sprite Small"; BasePrice=14m}
+let spriteM = {Size=Medium; Type=Soda; Name="Sprite Medium"; BasePrice=14m}
+let spriteL = {Size=Large; Type=Soda; Name="Sprite Large"; BasePrice=14m}
+
+//Milk
+let milkS = {Size=Small; Type=Milk; Name="Milk Small"; BasePrice=6m}
+let milkM = {Size=Medium; Type=Milk; Name="Milk Medium"; BasePrice=6m}
+let milkL = {Size=Large; Type=Milk; Name="Milk Large"; BasePrice=6m}
+
+let warmMilkS = {Size=Small; Type=Milk; Name="Warm Milk Small"; BasePrice=6.5m}
+let warmMilkM = {Size=Medium; Type=Milk; Name="Warm Milk Medium"; BasePrice=6.5m}
+let warmMilkL = {Size=Large; Type=Milk; Name="Warm Milk Large"; BasePrice=6.5m}
